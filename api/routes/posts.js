a17b92716c5f8034aa6af347a827b631bc0bc963
faheemjabbar <faheemjabbar36@gmail.com>
2025-08-13 00:35:49 +0500

    Initial Commit

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Post = require('../models/post');
+const Post = require('../models/Post');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
